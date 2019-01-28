f8c9d5f13589180e8ac66e60a5f4a99b94cd5948
chenpeiwen <chenpw@hzzh.com>
2019-01-28 15:01:34 +0800

    Junit

--- NyxCommon/src/main/java/junit/Junit.java
+++ NyxCommon/src/main/java/junit/Junit.java
@@ -13,11 +13,16 @@ public class Junit {
     public static void run(Class c) throws InstantiationException,
             IllegalAccessException, IllegalArgumentException,
             InvocationTargetException {
-        Method[] methods = c.getDeclaredMethods();//获取对象声明的方法集合
-        List<Method> testList = new ArrayList<Method>();//@Test注解的方法集合
-        Method afterMethod = null;//@After注解方法
-        Method beforeMethod = null;//@Before的注解方法
-        for (Method method : methods) {//循环对象声明的所有方法
+        //获取对象声明的方法集合
+        Method[] methods = c.getDeclaredMethods();
+        //@Test注解的方法集合
+        List<Method> testList = new ArrayList<Method>();
+        //@After注解方法
+        Method afterMethod = null;
+        //@Before的注解方法
+        Method beforeMethod = null;
+        //循环对象声明的所有方法
+        for (Method method : methods) {
             //如果有@Test注解，则将该方法加入到Test注解的方法集合
             if (method.isAnnotationPresent(Test.class)) {
                 testList.add(method);
