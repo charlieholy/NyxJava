a3bc32880f581a879b67bbcefabfd72bb242bf01
chenpeiwen <357483226@qq.com>
2019-02-23 10:11:58 +0800

    add mybatis demo

---
+++ MyxMyBatis/src/test/java/UserTest.java
@@ -0,0 +1,37 @@
+import com.User;
+import com.alibaba.fastjson.JSON;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class UserTest {
+    @Test
+    public void userFindByIdTest(){
+        //定义读取文件名
+        String resources = "mybatis-config.xml";
+        //创建流
+        Reader reader=null;
+        try {
+            //读取mybatis-config.xml文件到reader对象中
+            reader= Resources.getResourceAsReader(resources);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //初始化mybatis,创建SqlSessionFactory类的实例
+        SqlSessionFactory sqlMapper=new SqlSessionFactoryBuilder().build(reader);
+        //创建session实例
+        SqlSession session=sqlMapper.openSession();
+        //传入参数查询，返回结果
+        User user=session.selectOne("findById",2);
+        //输出结果
+        System.out.println(JSON.toJSON(user));
+        //关闭session
+        session.close();
+
+    }
+}
